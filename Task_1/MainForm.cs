7baeb54eb9540cf24942dd878bd14437f880b138
T500 <mks59k@gmail.com>
2018-08-18 18:08:59 +0300

    Создание коллекции обьектов.

--- Task_1/MainForm.cs
+++ Task_1/MainForm.cs
@@ -12,9 +12,27 @@ namespace Task_1
 {
     public partial class MainForm : Form
     {
+        List<Person> people = null;
+
         public MainForm()
         {
             InitializeComponent();
+
+            this.Load += MainForm_Load;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            this.people = new List<Person>()
+            {
+                new Person("Andrey", 24, "Kyiv"),
+                new Person("Liza", 18, "Moscow"),
+                new Person("Oleg", 15, "London"),
+                new Person("Sergey", 55, "Kyiv"),
+                new Person("Sergey", 32, "Kyiv")
+            };
+
+            this.dataGridViewAllData.DataSource = this.people;
         }
     }
 }
---
+++ Task_1/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+
+        public Person() :this("noname", 0, "no city") {}
+
+        public Person(string name, int age, string city)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.City = city;
+        }
+    }
+}
